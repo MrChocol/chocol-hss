13bc551318a4ac22c6277ca27eadd96e0c5eb9cd
chocolate <314159715@qq.com>
2018-09-12 23:09:16 +0800

    add user

---
+++ hss-user/src/main/java/com/chenli/hssuser/Model/Role.java
@@ -0,0 +1,10 @@
+package com.chenli.hssuser.Model;
+
+public class Role {
+    // 名称
+    private String name;
+    // 角色编号
+    private String sn;
+    // 角色和权限多对多关系
+//    private List<Permission> permissions = new ArrayList<Permission>();
+}
---
+++ hss-user/src/main/java/com/chenli/hssuser/Model/User.java
@@ -0,0 +1,116 @@
+package com.chenli.hssuser.Model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class User {
+    private String id;
+    private String username;
+    private String password;
+    private String salt = "bug";
+    private String realName;
+    private String tel;
+    private String email;
+    // 对应的角色
+    private Integer roleId;
+    // 对应用户绑定的微信号
+    private String fromUserName;
+    // 员工和角色多对多
+    private List<Role> roles = new ArrayList<Role>();
+
+//    // 处理中间表保存
+//    public List<Map<String, Long>> getRoleMap() {
+//        List<Map<String, Long>> list = new ArrayList<Map<String, Long>>();
+//        if (getRoles().size() > 0) {
+//            for (Role role : getRoles()) {
+//                Map<String, Long> map = new HashMap<String, Long>();
+//                map.put("userId", getId());
+//                map.put("roleId", role.getId());
+//                list.add(map);
+//            }
+//        }
+//        return list;
+//    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getFromUserName() {
+        return fromUserName;
+    }
+
+    public void setFromUserName(String fromUserName) {
+        this.fromUserName = fromUserName;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+}
